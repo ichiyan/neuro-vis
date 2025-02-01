e5f4472dd1387fc50b6646d20c17cf61b1072f73
e_lll <gabriela.balisacan01@gmail.com>
2025-02-02 06:31:24 +0800

    initial restructure

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Published Paper: https://dl.acm.org/doi/10.1145/3661725.3661744
 
 ## Notes:
 
-**August 2024**: The source code and supplementary materials will be released in this repository after a few weeks.  
+Code from Paperspace and other repos is currently being restructured, cleaned up, and added here. 
---
+++ src/neurovis/setup_recons.sh
@@ -0,0 +1,23 @@
+set -e
+
+conda create -n neurovis_recons python=3.10.8 -y
+conda activate neurovis_recons
+
+pip install -U git+https://github.com/huggingface/diffusers.git 
+
+pip install transformers accelerate safetensors clip open-clip-torch dalle2-pytorch typing-extensions controlnet_aux 
+
+pip install einops torchvision omegaconf torchtext ml-collections gdown webdataset braceexpand
+
+pip install nltk
+
+pip install pytorch_lightning basicsr
+
+pip install torch torchaudio
+
+pip install ipykernel pickleshare jupyter
+
+pip install gdown==5.1.0
+
+pip install git+https://github.com/tencent-ailab/IP-Adapter.git
+
