bce8cdbe9bfcbf098e3c721cd94331b994c95597
e_lll <gabriela.balisacan01@gmail.com>
2025-02-03 04:52:17 +0800

    recons ntbk

---
+++ src/definitions.py
@@ -0,0 +1,4 @@
+# import os 
+
+# SRC_DIR = os.path.dirname(os.path.abspath(__file__))
+# DATA_DIR = os.path.join(SRC_DIR, 'data') 
--- src/neurovis/setup_recons.sh
+++ src/neurovis/setup_recons.sh
@@ -5,7 +5,9 @@ conda activate neurovis_recons
 
 pip install -U git+https://github.com/huggingface/diffusers.git 
 
-pip install transformers accelerate safetensors clip open-clip-torch dalle2-pytorch typing-extensions controlnet_aux 
+pip install transformers==4.27.0
+
+pip install accelerate safetensors clip open-clip-torch dalle2-pytorch typing-extensions controlnet_aux 
 
 pip install einops torchvision omegaconf torchtext ml-collections gdown webdataset braceexpand
 
@@ -20,4 +22,7 @@ pip install ipykernel pickleshare jupyter
 pip install gdown==5.1.0
 
 pip install git+https://github.com/tencent-ailab/IP-Adapter.git
+
+pip install salesforce-lavis
+
 
